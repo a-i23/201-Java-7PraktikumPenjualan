a6b5cecef98cb9d0ad7a85502ebd85c00c314ceb
Ahmad Ihwan <ahmad.ihwan2305@gmail.com>
2021-02-15 02:31:59 +0800

    mengisi atribut

---
+++ src/main/java/com/ahmadihwan/praktikumpenjualan/model/JenisBarang.java
@@ -0,0 +1,12 @@
+package com.ahmadihwan.praktikumpenjualan.model;
+
+import java.sql.Connection;
+
+public class JenisBarang {
+    Connection con;
+    
+    private int id;
+    private String namaJenisBarang;
+
+    
+}
