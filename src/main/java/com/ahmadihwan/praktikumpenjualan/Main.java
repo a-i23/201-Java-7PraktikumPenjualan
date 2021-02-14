904cc84924a6a8d5707cf5b535b3cf92ed8cb72b
Ahmad Ihwan <ahmad.ihwan2305@gmail.com>
2021-02-15 01:49:59 +0800

    Uji koneksi ke database

--- src/main/java/com/ahmadihwan/praktikumpenjualan/Main.java
+++ src/main/java/com/ahmadihwan/praktikumpenjualan/Main.java
@@ -1,6 +1,13 @@
 package com.ahmadihwan.praktikumpenjualan;
+
+import com.ahmadihwan.praktikumpenjualan.db.Database;
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        Database db = new Database();
+        Connection con = db.getConnection();
     }
 }
