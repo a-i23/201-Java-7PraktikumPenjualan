19ef1c11b6789abaa55b53b24576ec8d646afe7d
Ahmad Ihwan <ahmad.ihwan2305@gmail.com>
2021-02-15 12:51:41 +0800

    testing read data

--- src/main/java/com/ahmadihwan/praktikumpenjualan/Main.java
+++ src/main/java/com/ahmadihwan/praktikumpenjualan/Main.java
@@ -3,6 +3,7 @@ package com.ahmadihwan.praktikumpenjualan;
 import com.ahmadihwan.praktikumpenjualan.db.Database;
 import com.ahmadihwan.praktikumpenjualan.model.JenisBarang;
 import java.sql.Connection;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +13,11 @@ public class Main {
         Connection con = db.getConnection();
         
         JenisBarang jenisBarang = new JenisBarang(con);
-        jenisBarang.setNamaJenisBarang("TESTING namajenisbarang");
-        jenisBarang.create();
+//        jenisBarang.setNamaJenisBarang("TESTING namajenisbarang");
+//        jenisBarang.create();
+
+        ArrayList<Object> list = jenisBarang.read();
+        JenisBarang jb = (JenisBarang) list.get(1);
+        System.out.println(jb.getNamaJenisBarang());
     }
 }
